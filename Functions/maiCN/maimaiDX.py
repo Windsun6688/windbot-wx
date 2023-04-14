d8944f61f6df4a003f9e1ba51dcb02f5e87ddb95
Windsun_ <48640109+Windsun6688@users.noreply.github.com>
2023-04-14 17:10:24 +0800

    Add diving-fish data source

--- Functions/maiCN/maimaiDX.py
+++ Functions/maiCN/maimaiDX.py
@@ -1,9 +1,8 @@
-import json,os,pprint,requests
+import json,os,requests
 from rapidfuzz import fuzz, utils
 from ..sqlHelper import *
 
-maiJson = open("maidata.json",encoding='UTF-8').read()
-maiData = json.loads(maiJson)
+static = os.path.join(os.path.dirname(__file__),'resource')
 
 scoreRank = ['d', 'c', 'b', 'bb', 'bbb', 'a', 'aa', 'aaa', 's', 's+', 'ss', 'ss+', 'sss', 'sss+']
 comboRank = ['fc', 'fc+', 'ap', 'ap+']
@@ -17,63 +16,6 @@ levelList = ['1', '2', '3', '4', '5', '6', '7', '7+', '8', '8+', '9', '9+', '10'
 achievementList = [50.0, 60.0, 70.0, 75.0, 80.0, 90.0, 94.0, 97.0, 98.0, 99.0, 99.5, 100.0, 100.5]
 BaseRa = [0.0, 5.0, 6.0, 7.0, 7.5, 8.5, 9.5, 10.5, 12.5, 12.7, 13.0, 13.2, 13.5, 14.0]
 BaseRaSpp = [7.0, 8.0, 9.6, 11.2, 12.0, 13.6, 15.2, 16.8, 20.0, 20.3, 20.8, 21.1, 21.6, 22.4]
-
-def maisearch(datalist,callerid,roomid = None):
-    if datalist[0].lower() == 'f':
-        keyword = ''
-        for word in datalist[1:]:
-            keyword += (word + ' ')
-        keyword = keyword[:-1]
-        results = []
-        for song in maiData:
-            if fuzz.QRatio(keyword,song['title']) >= 65:
-                results.append(song)
-    else:
-        keyword = ''
-        for word in datalist[0:]:
-            keyword += (word + ' ')
-        keyword = keyword[:-1]
-        results = []
-        for song in maiData:
-            if fuzz.QRatio(keyword,song['title']) >= 90:
-                results.append(song)
-                break
-
-    if len(results) == 0:
-        return ['没有搜寻到结果,或搜索模式关键词错误。']
-    elif len(results) > 5:
-        return ['请尝试优化搜索词。']
-
-    reply_txt = f"共找到以下{len(results)}个结果:\n"
-    for s in results:
-        output(s,background = 'MINT')
-        isDX = '[DX]'
-        title = s['title']
-        artist = s['artist']
-        category = s['category']
-        remas_txt = ''
-
-        try:
-            bas_lvl = s['dx_lev_bas']
-            adv_lvl = s['dx_lev_adv']
-            exp_lvl = s['dx_lev_exp']
-            mas_lvl = s['dx_lev_mas']
-            if len(s) > 9:
-                remas_txt = f" | ReMas{s['dx_lev_remas']}"
-        except KeyError as e:
-            # is STD song
-            isDX = '[STD]'
-            bas_lvl = s['lev_bas']
-            adv_lvl = s['lev_adv']
-            exp_lvl = s['lev_exp']
-            mas_lvl = s['lev_mas']
-            if len(s) > 9:
-                remas_txt = f" | ReMas{s['lev_remas']}"
-
-        version = s['version']
-        reply_txt += f"{isDX}{artist} - {title}\n-版本:{version} | 分区:{category}\n--Bas{bas_lvl} | Adv{adv_lvl} | Exp{exp_lvl} | Mas{mas_lvl}{remas_txt}\n"
-
-    return [reply_txt]
 
 def mai_api_get(gamertag):
     get_type = ['player']
@@ -102,6 +44,7 @@ def process_record(playrecord: dict):
     acc = playrecord['achievements']
     racc = playrecord['dxScore']
     rate = playrecord['rate'].upper().replace('P','+')
+    rating = playrecord['ra']
     fc = playrecord['fc']
     fs = playrecord['fs']
     fc_txt = ''
@@ -111,4 +54,82 @@ def process_record(playrecord: dict):
     if fs:
         fs_txt = full_sync_rank[sync_rank.index(fs)]
 
-    return f"\n[{chart_type}]{title} | {level} {chart_const}\n{acc}({rate}) DXScore: {racc}\n{fc_txt}{fs_txt}\n"
+    return f"\n[{chart_type}]{title} | {level} {chart_const}\n{acc}({rate}) Rating:{rating} DXScore:{racc}\n{fc_txt}{fs_txt}\n"
+
+def mai_music_get():
+    # SONG DETAILS
+    resp = requests.get('https://www.diving-fish.com/api/maimaidxprober/music_data')
+    if resp.status_code != 200:
+        output('maimaiDX曲目数据获取失败,切换至本地暂存文件','WARNING',background = 'WHITE')
+        with open(os.path.join(static, 'music_data.json'), 'r', encoding='utf-8') as f:
+            songs = json.loads(f.read())
+    else:
+        songs = resp.json()
+        with open(os.path.join(static, 'music_data.json'), 'w', encoding='utf-8') as f:
+            f.write(json.dumps(songs, ensure_ascii=False, indent=4))
+
+    # CHART DETAILS
+    resp = requests.get('https://www.diving-fish.com/api/maimaidxprober/chart_stats')
+    if resp.status_code != 200:
+        output('maimaiDX谱面数据获取失败,切换至本地暂存文件','WARNING',background = 'WHITE')
+        with open(os.path.join(static, 'chart_stats.json'), 'r', encoding='utf-8') as f:
+            stats = json.loads(f.read())
+    else:
+        stats = resp.json()
+        with open(os.path.join(static, 'chart_stats.json'), 'w', encoding='utf-8') as f:
+            f.write(json.dumps(stats, ensure_ascii=False, indent=4))
+    return songs,stats
+
+mai_songs,mai_charts = mai_music_get()
+
+def maisearch(datalist,callerid,roomid = None):
+    results = []
+    if datalist[0].lower() == 'f':
+        keyword = ''
+        for word in datalist[1:]:
+            keyword += (word + ' ')
+        keyword = keyword[:-1]
+        for song in mai_songs:
+            # print(song)
+            if fuzz.QRatio(keyword,song['title']) >= 65:
+                results.append(song)
+    else:
+        keyword = ''
+        for word in datalist[0:]:
+            keyword += (word + ' ')
+        keyword = keyword[:-1]
+        for song in mai_songs:
+            if fuzz.QRatio(keyword,song['title']) >= 90:
+                results.append(song)
+                break
+
+    if len(results) == 0:
+        return ['没有搜寻到结果,或搜索模式关键词错误。']
+    elif len(results) > 5:
+        return ['请尝试优化搜索词。']
+
+    reply_txt = f"共找到以下{len(results)}个结果:"
+    for s in results:
+        # output(s,background = 'MINT')
+        sid = s['id']
+        is_DX = s['type']
+        title = s['title']
+
+        artist = s['basic_info']['artist']
+        version = s['basic_info']['from']
+        category = s['basic_info']['genre']
+        bpm = s['basic_info']['bpm']
+        ds = s['ds']
+
+        new_txt = ''
+        if s['basic_info']['is_new']:
+            new_txt = '[NEW]'
+
+        remas_txt = '\n'
+        if len(ds) == 5:
+            remas_txt = f' | ReMas{ds[4]}'
+
+        reply_txt += f"\n[{is_DX}]{new_txt} {artist} - {title}\n-版本:{version} | 分区:{category} | BPM:{bpm}\n--Bas{ds[0]} | Adv{ds[1]} | Exp{ds[2]} | Mas{ds[3]}{remas_txt}"
+
+    return [reply_txt]
+
