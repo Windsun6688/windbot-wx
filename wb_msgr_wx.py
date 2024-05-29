9aaf2f96589b1731582cdb364c174c81da76c43c
Windsun <48640109+Windsun6688@users.noreply.github.com>
2024-05-29 21:04:47 +0800

    waterproof tape patch: output

--- wb_msgr_wx.py
+++ wb_msgr_wx.py
@@ -168,3 +168,60 @@ class Messenger(object):
             'ext': 'null',
         }
         self.ws.send(json.dumps(ws_data))
+
+# Custom Print Wrapper
+def output(msg, logtype='SYSTEM', mode='DEFAULT', background='DEFAULT'):
+    LogColor = {
+        'SYSTEM': '034',
+        'ERROR': '037',
+        'GROUPCHAT': '036',
+        'DM' : '033',
+        'HEART_BEAT': '035',
+        'PAT': '037',
+        'SEND': '032',
+        'CALL' : '031',
+        'WARNING': '031',
+        'CREATE_LINK':'032',
+        'STOP_LINK':'031',
+        'RSS': '037'
+    }
+    LogMode = {
+        'DEFAULT': '0',
+        'HIGHLIGHT': '1',
+        'UNDERLINE': '4'
+    }
+    LogBG = {
+        'DEFAULT': '',
+        'RED' : ';41',
+        'YELLOW' : ';43',
+        'BLUE' : ';44',
+        'WHITE' : ';47',
+        'GREEN' : ';42',
+        'MINT' : ';46',
+        'PURPLE' : ';45'
+    }
+    color = LogColor.get(logtype)
+    mode = LogMode.get(mode)
+    bg = LogBG.get(background)
+
+    now = time.strftime("%Y-%m-%d %X")
+
+    # Shorten logs of too long messages
+    line_cnt = msg.count('\n') + 1
+    if line_cnt > 10 and logtype != 'ERROR':
+        msg = "\n".join(msg.split("\n")[:10])
+        msg += '\n......'
+
+    print(f"[{now} \033[{mode};{color}{bg}m{logtype}\033[0m] {msg}")
+
+    # Write Error Logs on to Local File
+    # if logtype == 'ERROR':
+    #     error_log_file = open('ErrorLog.txt','a')
+    #     error_log_file.write(f"[{now} {logtype}] {msg}\n")
+    #     error_log_file.close()
+
+    # Store Log into latest_logs list
+    # if logtype != 'HEART_BEAT':
+    #     if len(latest_logs) == 20:
+    #         latest_logs.pop(0)
+    #     latest_logs.append(f"[{now} {logtype}] {msg}")
